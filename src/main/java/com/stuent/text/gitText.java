2c35407e9b78238e89943fe4948514be61684ba8
Big_cat <big_cat1555@163.com>
2021-10-22 16:52:09 +0800

    first commit

---
+++ src/main/java/com/stuent/text/gitText.java
@@ -0,0 +1,11 @@
+package com.stuent.text;
+
+/**
+ * @author DongNing
+ * @create 2021-10-22 16:47
+ */
+public class gitText {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
