bf45275cf8cb72424e44c34dca72d7c4876b2a82
Big_cat <big_cat1555@163.com>
2021-10-22 16:56:06 +0800

    second commit

--- src/main/java/com/stuent/text/gitText.java
+++ src/main/java/com/stuent/text/gitText.java
@@ -2,10 +2,10 @@ package com.stuent.text;
 
 /**
  * @author DongNing
- * @create 2021-10-22 16:47
  */
 public class gitText {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
     }
 }
